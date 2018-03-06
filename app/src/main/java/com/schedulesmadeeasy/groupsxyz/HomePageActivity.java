7e1313e034c8629ad1cf1a62026ba728eefee447
Antwonn <agaa2492@hotmail.com>
2018-03-05 17:02:53 -0800

    Added appbard and started on recycler view with cardview.

--- app/src/main/java/com/schedulesmadeeasy/groupsxyz/HomePageActivity.java
+++ app/src/main/java/com/schedulesmadeeasy/groupsxyz/HomePageActivity.java
@@ -1,36 +1,71 @@
 package com.schedulesmadeeasy.groupsxyz;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class HomePageActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+
+                        return true;
+                    }
+                }
+        );
+
+        RecyclerView recyclerView = findViewById(R.id.group_recycler_view);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+        //TODO ADD A LIST ADAPTER
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.home_page_activity_menu, menu);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.action_settings:
+            case android.R.id.home:
                 //USER CHOSE SETTINGS ITEM, CHANGE TO APP SETTINGS SCREEN
-
+                mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
 
             default:
